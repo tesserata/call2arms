cb7cf4972816f2504a944f9b3b3a8f7006a0af85
tesserata <daria.silman@gmail.com>
2026-04-16 23:37:10 +0300

    clean up naming

--- call2arms/message.py
+++ call2arms/message.py
@@ -7,7 +7,7 @@ $party_tag Do we play at <t:$ts:f>?
 """
 )
 
-def get_timestamp() -> int:
+def get_next_session_timestamp() -> int:
     now = datetime.now(timezone.utc)
     target = (now + timedelta(days=(5 - now.weekday()) % 7)).replace(
         hour=15, minute=0, second=0, microsecond=0
@@ -20,5 +20,5 @@ def get_timestamp() -> int:
 def get_session_message(party_tag: str) -> str:
     return SESSION_MESSAGE.substitute(
         party_tag=party_tag,
-        ts=get_timestamp(),
+        ts=get_next_session_timestamp(),
     )
